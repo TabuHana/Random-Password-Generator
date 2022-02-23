b46a987479491e066b0193ee6dd0b58b25955a28
Tabu Hana <taboodog@hotmail.com>
2022-02-22 21:45:19 -0800

    intial

---
+++ Develop/script.js
@@ -0,0 +1,17 @@
+/*
+  Feature dynamically updated HTML and CSS powered by js
+  Clean, polished UI and responsive, to multi screen sizes
+
+  ======= ACCEPTANCE CRITERIA=======
+  -when click button, pass generated
+  -prompted for pass criteria?
+  -select criteria 
+  - pass length of min 8 max 128
+  -choose character types (upper, lower, numeria, special)
+  -after all prompts answered input validated???
+  -generate password that matches criteria
+  -pass displayed in alert or on page
+*/
+document.getElementById('generate').addEventListener('click', () => {
+
+})
